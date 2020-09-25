d088c6f25f640e5eb65ebd5bed20f2137835a003
jose <jose@localhost>
2020-09-25 12:59:59 +0200

    Proyecto configurado como Maven con la dependencia de MySQL JDBC. Ventana básica creada, main separado y modelo usuario creado.

---
+++ evaluacioninicial/src/GUI/Login.java
@@ -0,0 +1,26 @@
+package GUI;
+
+import javax.swing.JFrame;
+
+public class Login {
+
+	private JFrame frame;
+
+	/**
+	 * Create the application.
+	 */
+	public Login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
---
+++ evaluacioninicial/src/Main/StartUp.java
@@ -0,0 +1,14 @@
+package Main;
+
+import GUI.Login;
+
+public class StartUp {
+
+	public static void main(String[] args) {
+	
+		//Creo objeto venta Login y eso lanza todo el proceso de creación de GUI
+		new Login();
+
+	}
+
+}
